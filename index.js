de7ecdf3f8c660122109a1564bf2f1577254d7bf
xihomararazo <xihomararazo@gmail.com>
2023-02-06 16:21:34 -0600

    Se creo la Clase A y se agrego un ejemplo de cómo se debe utilizar.

---
+++ index.js
@@ -0,0 +1,7 @@
+var getParametros = function (obj) { return Object.values(obj); };
+// ejemplo 
+var exampleClaseA = {
+    foo: "foo",
+    bar: "bar"
+};
+console.log(getParametros(exampleClaseA));
